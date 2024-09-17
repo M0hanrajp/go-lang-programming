d8c77af1ccb4d74ad494cd051cd64dda178dd7b8
M0hanrajp <mohanrajp.github@gmail.com>
2024-09-17 12:06:25 +0530

    Functions and return types

--- basics/playing-cards-project/main.go
+++ basics/playing-cards-project/main.go
@@ -1,30 +1,20 @@
+// This code is act as the main code for the project
+// the project.go is commented version of the code where multiple experiments are done
 package main
 
 import "fmt"
 
+//	func main() {
+//		card := newCard()
+//		fmt.Printf("%s\n", card)
+//	}
+//
+//	func newCard() string {
+//		return "Five of diamonds"
+//	}
+//
+// the below funciton is to test project.go
 func main() {
-
-	var card = "Ace of spades"
-	// syntax
-	// var -> you are declaring a variable
-	// card -> name of the variable
-	// string -> data type of the variable
-	// = -> asign a valid data to the variable card
-	// var card = "Ace of spades" [ No error reported by the compiler ]
-	// card = "Ace of spades", -> undefiend card
-
-	// another way of decalring variables
-	cardOne := "Five of hearts"
-	// syntax
-	// := -> create a variable name on the LHS and assign the value of RHS to it
-
-	// When assigning a new value to previous variable then use `=`
-	cardOne = "Five of diamonds"
-
-	fmt.Println(card)
-	fmt.Println(cardOne)
+	fmt.Println("Commented code execution:")
+	MainCommented()
 }
-
-// Note:
-// Go is statically typed language, https://stackoverflow.com/questions/1517582/what-is-the-difference-between-statically-typed-and-dynamically-typed-languages
-// The types are checked before running (static) and the type error is immediately caught
